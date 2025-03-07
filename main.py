5ad5a1f1d7e978e685252e605679d669cf1c4606
Jakob Kristensen <jakobk205@gmail.com>
2025-03-07 09:50:42 +0100

    Set up Arduino board and pins in main.py

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import pygame
 import random
 import time
 import pyfirmata 
+
+# Set up the Arduino board
+board = pyfirmata.Arduino('COM3')
+it = pyfirmata.util.Iterator(board)
+it.start()
+
+# Set up the pins
+led_pin = board.get_pin()
+potentiometer_pin = board.get_pin()
+
